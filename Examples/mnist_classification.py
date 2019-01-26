a27e63beaf53ca93537d0dd4594fb6166305fbd3
hezopoy <hezopoy@gmail.com>
2019-01-26 23:28:39 +0700

    Update mnist_classification.py

--- Examples/mnist_classification.py
+++ Examples/mnist_classification.py
@@ -10,34 +10,44 @@ from keras.datasets import mnist
 from keras import models
 from keras import layers
 
-'''
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+#network = models.load_model('mnist.md')
 network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 network.add(layers.Dense(10, activation='softmax'))
 network.compile(optimizer='rmsprop',
 loss='categorical_crossentropy',
 metrics=['accuracy'])
-'''
-network = models.load_model("mnist.md")
-train_images = train_images.reshape((60000, 28 * 28))
-train_images = train_images.astype('float32') / 255
-test_images = test_images.reshape((10000, 28 * 28))
-test_images = test_images.astype('float32') / 255
-'''
-x_val = train_images[:10000]
-partial_x_train = train_images[10000:]
-y_val = train_labels[:10000]
-partial_y_train = train_labels[10000:]
-history = network.fit(partial_x_train,
-partial_y_train,
-epochs=20,
-batch_size=512,
-validation_data=(x_val, y_val))
-'''
 from keras.utils import to_categorical
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
-#network.fit(train_images, train_labels, epochs=5, batch_size=128)
-test_loss, test_acc = network.evaluate(test_images, test_labels)
-print (test_acc)
+history = network.fit(train_images, train_labels, epochs=7, batch_size=128, validation_data=(test_images,test_labels))
+#test_loss, test_acc = network.evaluate(test_images, test_labels)
+#print('test_acc:', test_acc)
 network.save("mnist.md")
+import matplotlib.pyplot as plt
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(history_dict['acc']) + 1)
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+plt.clf()
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+plt.plot(epochs, acc_values, 'bo', label='Training acc')
+plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
